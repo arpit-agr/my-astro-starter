d3fcf852715bd675a84792a6750867988f1b2e56
arpit-agr <arpit.agrawal2502@gmail.com>
2026-02-27 19:21:26 +0530

    generate design tokens

---
+++ sugarcube.config.ts
@@ -0,0 +1,71 @@
+import { defineConfig } from '@sugarcube-sh/cli';
+
+export default defineConfig({
+	resolver: 'src/design-tokens/tokens.resolver.json',
+	output: {
+		components: 'src/styles/blocks',
+	},
+	utilities: {
+		padding: {
+			source: 'space.*',
+			prefix: 'p',
+			directions: ['all', 'top', 'right', 'bottom', 'left', 'x', 'y'],
+		},
+		margin: {
+			source: 'space.*',
+			prefix: 'm',
+			directions: ['all', 'top', 'right', 'bottom', 'left', 'x', 'y'],
+		},
+		'--flow-space': {
+			source: 'space.*',
+			prefix: 'flow-space',
+		},
+		'--region-space': {
+			source: 'space.*',
+			prefix: 'region-space',
+		},
+		'--cluster-gap': {
+			source: 'space.*',
+			prefix: 'cluster-gap',
+		},
+		'--switcher-gap': {
+			source: 'space.*',
+			prefix: 'switcher-gap',
+		},
+		'--grid-gap': {
+			source: 'space.*',
+			prefix: 'grid-gap',
+		},
+		'--wrapper-max-width': {
+			source: 'container.*',
+			prefix: 'wrapper-max-width',
+		},
+		gap: {
+			source: 'space.*',
+			prefix: 'gap',
+		},
+		color: {
+			source: 'color.*',
+			prefix: 'text',
+			stripDuplicates: true,
+		},
+		'background-color': {
+			source: 'color.*',
+			prefix: 'bg',
+		},
+		'font-size': {
+			source: 'text.*',
+		},
+		'font-weight': {
+			source: 'font.weight.*',
+			prefix: 'font-weight',
+		},
+		'letter-spacing': {
+			source: 'tracking.*',
+		},
+		'border-radius': {
+			source: 'radius.*',
+			prefix: 'rounded',
+		},
+	},
+});
